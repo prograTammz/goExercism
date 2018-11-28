4254fe795f37c064698c730599a0243a7daf356c
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2018-11-28 17:41:29 +0200

    first Exercise

---
+++ two-fer/two_fer.go
@@ -0,0 +1,13 @@
+package twofer
+
+import (
+	"fmt"
+)
+
+// ShareWith should have a comment documenting it.
+func ShareWith(name string) string {
+	if name != "" {
+		return fmt.Sprintf("One for %v, one for me.", name)
+	}
+	return "One for you, one for me."
+}
