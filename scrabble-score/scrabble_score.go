cd25487effd12bd7580ee3a3fbb6a7827b52fea6
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2018-12-02 19:50:13 +0200

    added scrabble-score exercise

---
+++ scrabble-score/scrabble_score.go
@@ -0,0 +1,51 @@
+package scrabble
+
+import (
+	"strings"
+)
+
+//Score functions loops over each word and calculate it's score from the
+//contained letters at which each letter has a score.
+func Score(word string) int {
+
+	letter := map[string]int{
+		"a": 1,
+		"e": 1,
+		"i": 1,
+		"o": 1,
+		"u": 1,
+		"l": 1,
+		"n": 1,
+		"r": 1,
+		"s": 1,
+		"t": 1,
+		//the value "2" letters
+		"d": 2,
+		"g": 2,
+		//the value 3 letters
+		"b": 3,
+		"c": 3,
+		"m": 3,
+		"p": 3,
+		//the value 4 letters
+		"f": 4,
+		"h": 4,
+		"v": 4,
+		"w": 4,
+		"y": 4,
+		//the value 5 letters
+		"k": 5,
+		//the value 8 letters
+		"j": 8,
+		"x": 8,
+		//the value 10 letter
+		"q": 10,
+		"z": 10,
+	}
+	result := strings.ToLower(word)
+	var count int
+	for _, char := range result {
+		count += letter[string(char)]
+	}
+	return count
+}
