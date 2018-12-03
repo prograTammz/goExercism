67d435c23cde7e28eebb2dd91b3bc5de36e7349f
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2018-12-03 08:11:33 +0200

    refactored scrabble-score

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,51 +1,55 @@
 package scrabble
 
 import (
-	"strings"
+	"unicode"
 )
+
+var letter = map[rune]int{
+	'a': 1,
+	'e': 1,
+	'i': 1,
+	'o': 1,
+	'u': 1,
+	'l': 1,
+	'n': 1,
+	'r': 1,
+	's': 1,
+	't': 1,
+	//the value "2" letters
+	'd': 2,
+	'g': 2,
+	//the value 3 letters
+	'b': 3,
+	'c': 3,
+	'm': 3,
+	'p': 3,
+	//the value 4 letters
+	'f': 4,
+	'h': 4,
+	'v': 4,
+	'w': 4,
+	'y': 4,
+	//the value 5 letters
+	'k': 5,
+	//the value 8 letters
+	'j': 8,
+	'x': 8,
+	//the value 10 letter
+	'q': 10,
+	'z': 10,
+}
 
 //Score functions loops over each word and calculate it's score from the
 //contained letters at which each letter has a score.
 func Score(word string) int {
 
-	letter := map[string]int{
-		"a": 1,
-		"e": 1,
-		"i": 1,
-		"o": 1,
-		"u": 1,
-		"l": 1,
-		"n": 1,
-		"r": 1,
-		"s": 1,
-		"t": 1,
-		//the value "2" letters
-		"d": 2,
-		"g": 2,
-		//the value 3 letters
-		"b": 3,
-		"c": 3,
-		"m": 3,
-		"p": 3,
-		//the value 4 letters
-		"f": 4,
-		"h": 4,
-		"v": 4,
-		"w": 4,
-		"y": 4,
-		//the value 5 letters
-		"k": 5,
-		//the value 8 letters
-		"j": 8,
-		"x": 8,
-		//the value 10 letter
-		"q": 10,
-		"z": 10,
+	var result []rune
+	for _, val := range word {
+		result = append(result, unicode.ToLower(val))
 	}
-	result := strings.ToLower(word)
 	var count int
 	for _, char := range result {
-		count += letter[string(char)]
+		count += letter[char]
 	}
 	return count
 }
