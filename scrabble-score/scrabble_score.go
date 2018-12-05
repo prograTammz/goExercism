acd3bfd4b56136f4815ade1b5aeac0832913d94a
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2018-12-05 19:42:09 +0200

    refactored scrabble-score

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -4,52 +4,30 @@ import (
 	"unicode"
 )
 
-var letter = map[rune]int{
-	'a': 1,
-	'e': 1,
-	'i': 1,
-	'o': 1,
-	'u': 1,
-	'l': 1,
-	'n': 1,
-	'r': 1,
-	's': 1,
-	't': 1,
-	//the value "2" letters
-	'd': 2,
-	'g': 2,
-	//the value 3 letters
-	'b': 3,
-	'c': 3,
-	'm': 3,
-	'p': 3,
-	//the value 4 letters
-	'f': 4,
-	'h': 4,
-	'v': 4,
-	'w': 4,
-	'y': 4,
-	//the value 5 letters
-	'k': 5,
-	//the value 8 letters
-	'j': 8,
-	'x': 8,
-	//the value 10 letter
-	'q': 10,
-	'z': 10,
-}
-
 //Score functions loops over each word and calculate it's score from the
 //contained letters at which each letter has a score.
 func Score(word string) int {
 
-	var result []rune
-	for _, val := range word {
-		result = append(result, unicode.ToLower(val))
-	}
 	var count int
-	for _, char := range result {
-		count += letter[char]
+	for _, val := range word {
+		result := unicode.ToLower(val)
+		switch result {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+			count++
+		case 'd', 'g':
+			count += 2
+		case 'b', 'c', 'm', 'p':
+			count += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			count += 4
+		case 'k':
+			count += 5
+		case 'j', 'x':
+			count += 8
+		case 'q', 'z':
+			count += 10
+		}
+
 	}
 	return count
 }
